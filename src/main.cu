a0058b3a592eb62b4cb22a277611d0677ec04c84
Salvatore Di Gennaro <salvatore.digennaro@outlook.it>
2025-06-22 11:00:02 +0200

    Test

--- src/main.cu
+++ src/main.cu
@@ -4,8 +4,6 @@
 #include <assert.h>
 #include <math.h>
 
-#define COUNT 32
-
 __global__ void foo(float *values)
 {
     values[threadIdx.x] += 1;
@@ -13,11 +11,14 @@ __global__ void foo(float *values)
 
 int main(int argc, char const *argv[])
 {
+    assert(argc > 1);
+    int count = atoi(argv[1]);
+
     int gpu_count;
     cudaGetDeviceCount(&gpu_count);
     assert(gpu_count > 0);
 
-    int N = gpu_count * COUNT;
+    int N = gpu_count * count;
     float *host_values;
     cudaMallocHost(&host_values, N * sizeof(float));
     for (int i = 0; i < N; i++)
@@ -30,12 +31,12 @@ int main(int argc, char const *argv[])
 
         cudaSetDevice(i);
         cudaStreamCreate(&(streams[i]));
-        cudaMallocAsync(&dev_values, COUNT * sizeof(float), streams[i]);
-        cudaMemcpyAsync(dev_values, host_values + i * COUNT, COUNT * sizeof(float), cudaMemcpyHostToDevice, streams[i]);
+        cudaMallocAsync(&dev_values, count * sizeof(float), streams[i]);
+        cudaMemcpyAsync(dev_values, host_values + i * count, count * sizeof(float), cudaMemcpyHostToDevice, streams[i]);
 
-        foo<<<1, COUNT>>>(dev_values);
+        foo<<<1, count>>>(dev_values);
 
-        cudaMemcpyAsync(host_values + i * COUNT, dev_values, COUNT * sizeof(float), cudaMemcpyDeviceToHost, streams[i]);
+        cudaMemcpyAsync(host_values + i * count, dev_values, count * sizeof(float), cudaMemcpyDeviceToHost, streams[i]);
         cudaFreeAsync(dev_values, streams[i]);
     }
 
